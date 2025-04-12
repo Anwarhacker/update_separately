dca05988e60a113eed4b83a79fcdebbf86af04fd
Anwarhacker <patelanwar647@gmail.com>
2025-04-12 18:50:18 +0530

    added commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Registration from "./components/Registration";
 import Update from "./components/Update";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login"; // 🆕 Import the Login component
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <div>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Registration />}></Route>
-            <Route path="/update/:id" element={<Update />}></Route>
-          </Routes>
-        </Router>
-      </div>
-    </>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Registration />} />
+          <Route path="/update/:id" element={<Update />} />
+          <Route path="/login" element={<Login />} /> {/* 🆕 Login Route */}
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
