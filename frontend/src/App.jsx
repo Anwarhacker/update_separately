af318bfe88b25365237eaee9148c15729a75bbbc
Anwarhacker <patelanwar647@gmail.com>
2025-04-20 16:19:48 +0530

    updatedc all

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,42 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Registration from "./components/Registration";
 import Update from "./components/Update";
-import Login from "./components/Login"; // 🆕 Import the Login component
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import PrivateRoute from "./components/PrivateRoute";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Registration />} />
-          <Route path="/update/:id" element={<Update />} />
-          <Route path="/login" element={<Login />} /> {/* 🆕 Login Route */}
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Navbar />
+        <div className="container mx-auto px-4 py-8">
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Registration />} />
+            <Route 
+              path="/" 
+              element={
+                <PrivateRoute>
+                  <Home />
+                </PrivateRoute>
+              } 
+            />
+            <Route 
+              path="/update/:id" 
+              element={
+                <PrivateRoute>
+                  <Update />
+                </PrivateRoute>
+              } 
+            />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
